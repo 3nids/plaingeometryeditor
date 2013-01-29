f0ed4f7aede5c24df0b0226e003a2394ce559354
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-01-29 15:56:43 +0100

    support new QgsMapToolIdentify

--- identifygeometry.py
+++ identifygeometry.py
@@ -12,42 +12,49 @@ from PyQt4.QtGui import *
 from qgis.core import *
 from qgis.gui import *
 
-class identifyGeometry(QgsMapToolEmitPoint):
+try:
+	from qgis.gui import QgsMapToolIdentify 
+except:
+	from qgis.gui import QgsMapTool as QgsMapToolIdentify 	
+
+class identifyGeometry(QgsMapToolIdentify):
 	def __init__(self, canvas):
 		self.canvas = canvas
-		QgsMapToolEmitPoint.__init__(self, canvas)
-		QObject.connect( self, SIGNAL( "canvasClicked" ), self.onCanvasClicked )
+		QgsMapToolIdentify.__init__(self, canvas)
 
-	def canvasPressEvent(self, mouseEvent):
-		point = self.toMapCoordinates( mouseEvent.pos() )
-		self.emit( SIGNAL( "canvasClicked" ), point, mouseEvent.button(), mouseEvent.modifiers() )
+	def canvasReleaseEvent(self, mouseEvent):
+		if hasattr(self,"identify"):
+			if self.identify(mouseEvent.x(),mouseEvent.y(), self.TopDownStopAtFirst, self.VectorLayer):
+				result = self.results().mVectorResults[0]
+				self.emit( SIGNAL( "geomIdentified" ), result.mLayer, result.mFeature)
+		else:
+			point = self.toMapCoordinates( mouseEvent.pos() )
+			layer = self.canvas.currentLayer()
+			if layer == None:
+				return
+			if layer.type() != QgsMapLayer.VectorLayer:
+				return
+			point = self.canvas.mapRenderer().mapToLayerCoordinates(layer, point)
+			pixTolerance = 6
+			mapTolerance = pixTolerance * self.canvas.mapUnitsPerPixel()
+			rect = QgsRectangle(point.x()-mapTolerance,point.y()-mapTolerance,point.x()+mapTolerance,point.y()+mapTolerance)
+
+			provider = layer.dataProvider()
+			provider.select([], rect, True, True)
+			subset = []
+			f = QgsFeature()
+			while (provider.nextFeature(f)):
+				subset.append(f)
+			if len(subset) == 0:
+				return
+			if len(subset) > 1:
+				idx = QgsSpatialIndex()
+				for f in subset:
+					idx.insertFeature(f)
+				nearest = idx.nearestNeighbor( point, 1 )
+				print nearest
+				layer.featureAtId(nearest[0],f, True, False)
+			self.emit( SIGNAL( "geomIdentified" ), layer, f)			
+			
+			
 		
-	def onCanvasClicked(self, point, button, modifiers):
-		layer = self.canvas.currentLayer()
-		if layer == None:
-			return
-		if layer.type() != QgsMapLayer.VectorLayer:
-			return
-		if button != Qt.LeftButton:
-			return
-		point = self.canvas.mapRenderer().mapToLayerCoordinates(layer, point)
-		pixTolerance = 6
-		mapTolerance = pixTolerance * self.canvas.mapUnitsPerPixel()
-		rect = QgsRectangle(point.x()-mapTolerance,point.y()-mapTolerance,point.x()+mapTolerance,point.y()+mapTolerance)
-
-		provider = layer.dataProvider()
-		provider.select([], rect, True, True)
-		subset = []
-		f = QgsFeature()
-		while (provider.nextFeature(f)):
-			subset.append(f)
-		if len(subset) == 0:
-			return
-		if len(subset) > 1:
-			idx = QgsSpatialIndex()
-			for f in subset:
-				idx.insertFeature(f)
-			nearest = idx.nearestNeighbor( point, 1 )
-			print nearest
-			layer.featureAtId(nearest[0],f, True, False)
-		self.emit( SIGNAL( "geomIdentified" ), layer, f)
